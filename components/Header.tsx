5a426f2ca8be770832fc0027dae9e3eb0d16b403
Aleksandre <gabeditebuli@gmail.com>
2023-02-17 21:38:58 +0400

    add: facebook, instagram links

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { EmailIcon, FacebookIcon, InstagramIcon, Logo } from "../icons";
+import { SOCIAL_LINK } from "../constants";
 
 export const Header = () => {
   return (
@@ -11,8 +12,8 @@ export const Header = () => {
       </div>
 
       <div className="flex-initial flex-row flex justify-around w-40 invisible md:visible self-center">
-        <InstagramIcon />
-        <FacebookIcon />
+        <a href={SOCIAL_LINK.instagram} target='_blank'><InstagramIcon /></a>
+        <a href={SOCIAL_LINK.facebook} target='_blank'><FacebookIcon /></a>
         <EmailIcon />
       </div>
     </div>
