6589de5ada00cf08c4432cc966846c396859826e
Aleksandre <gabeditebuli@gmail.com>
2023-02-20 00:05:20 +0400

    fix: adjusted spacing/mobile version

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -10,7 +10,7 @@ export const HeroSection = () => {
         </h1>
       </div>
 
-      <div className="mt-8 flex flex-row">
+      <div className="md:mt-8 mt-6 flex flex-row">
         <div className="flex-col mx-1">
           <div className=" mb-3">
             <Image
