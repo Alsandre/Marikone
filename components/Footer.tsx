2118235565224d3b09cc118149e049159105b8ad
Aleksandre <gabeditebuli@gmail.com>
2023-02-19 22:09:12 +0400

    Add “Message Sent” animation to the submit button

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { SOCIAL_LINK } from "../constants";
 import { EmailIcon, FacebookIcon, InstagramIcon, Logo } from "../icons";
 
 export const Footer = () => {
   return (
     <div className="md:mx-36 mx-12 py-4 flex items-center md:justify-around border-t border-dark-100 border-opacity-10 flex-col md:flex-row">
+      <div className="flex-initial flex-row flex justify-around w-40 py-5 md:hidden self-center">
+        <a href={SOCIAL_LINK.instagram} target='_blank' rel="noreferrer"><InstagramIcon /></a>
+        <a href={SOCIAL_LINK.facebook} target='_blank' rel="noreferrer"><FacebookIcon /></a>
+        <EmailIcon />
+      </div>
       <div className="flex-initial flex justify-around self-center">
         <h1 className="text-sm text-end font-extralight text-dark-200">
           2023 All rights reserved © MARIKONE LLC
