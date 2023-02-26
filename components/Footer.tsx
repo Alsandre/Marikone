a519344140478e0bd4f1e84d9d61b3a81b22f83c
Aleksandre <gabeditebuli@gmail.com>
2023-02-26 11:31:25 +0400

    FIX: mobile / footer layout

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ import { EmailIcon, FacebookIcon, InstagramIcon, Logo } from "../icons";
 export const Footer = () => {
   return (
     <div className="md:mx-36 mx-12 py-4 flex items-center md:justify-around border-t border-dark-100 border-opacity-10 flex-col md:flex-row">
-      <div id='contact-footer' className="flex-initial flex-row flex justify-around w-40 md:invisible self-center">
+      <div id='contact-footer' className="flex-initial flex-row flex justify-around w-40 md:invisible self-center mt-[6px] mb-[22px]">
         <a href={SOCIAL_LINK.instagram} target='_blank' rel="noreferrer"><InstagramIcon /></a>
         <a href={SOCIAL_LINK.facebook} target='_blank' rel="noreferrer"><FacebookIcon /></a>
         <a href="#contact-form"><EmailIcon /></a>
